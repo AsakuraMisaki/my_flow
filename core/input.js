7c0930f96d8f70ddc8ba8e6ece6c48a3241ef4cc
MISAKI <275684857@qq.com>
2024-11-05 21:17:11 +0800

    -

--- core/input.js
+++ core/input.js
@@ -1,23 +1,23 @@
 
-import { Assets, Loader, Resolver, extensions } from "pixi.js";
+import { Assets, Loader, Rectangle, Resolver, extensions } from "pixi.js";
 import { YAML } from "./yaml";
 
 //可能不会使用pixijs原生交互, 一是在多全屏layer情况下的触发问题, 二是使交互判定尽量一致, 如键盘/鼠标/手柄等
 class Input { 
   static async setup() {
-    let config = await Assets.load("../core/input.yaml");
+    let mapper = await Assets.load("../core/input.yaml");
     const pf = { passive: false };
     this.clearAllMotion(config);
     
-    // document.addEventListener("keydown", this._onKeyDown.bind(this));
-    // document.addEventListener("keyup", this._onKeyUp.bind(this));
-    // document.addEventListener("pointerdown", this._onPointerDown.bind(this));
-    // document.addEventListener("pointermove", this._onPointerMove.bind(this));
-    // document.addEventListener("pointerup", this._onPointerUp.bind(this));
-    // document.addEventListener("wheel", this._onWheel.bind(this), pf);
-    // window.addEventListener("blur", this.clear.bind(this));
-    // this.clear();
-    // this.setRepeatWait();
+    document.addEventListener("keydown", this._onKeyDown.bind(this));
+    document.addEventListener("keyup", this._onKeyUp.bind(this));
+    document.addEventListener("pointerdown", this._onPointerDown.bind(this));
+    document.addEventListener("pointermove", this._onPointerMove.bind(this));
+    document.addEventListener("pointerup", this._onPointerUp.bind(this));
+    document.addEventListener("wheel", this._onWheel.bind(this), pf);
+    window.addEventListener("blur", this.clear.bind(this));
+    this.clear(mapper);
+    this.setRepeatWait();
     // this.clampDirection();
   }
 
@@ -28,7 +28,8 @@ class Input {
     }
   }
 
-  static clear() {
+  static clear(mapper) {
+    this._mapper = mapper;
     this._currentState = new Map();
     this._gcing = new Map();
     this._pointer = { _x: 0, _y: 0, x: 0, y: 0, wx: 0, wy: 0, _wx: 0, _wy: 0, ids: new Map() };
@@ -54,24 +55,6 @@ class Input {
       this._gcing.set(key, data);
     }
     this._currentState.delete(key);
-    // const data1 = this.getMapper()[key];
-    // if(data1){
-    //     for(let k in data1){
-    //         console.log(k)
-    //         this._onKeyUp({ key:k })
-    //     }
-    // }
-
-  }
-
-  static pointerCoor(e) {
-    this._pointer._x = this._pointer.x;
-    this._pointer._y = this._pointer.y;
-    const x = Graphics.pageToCanvasX(e.x); //240718 多点触碰需要测试和修改
-    const y = Graphics.pageToCanvasY(e.y);
-    this._pointer.x = x;
-    this._pointer.y = y;
-    return { x, y };
   }
 
   static pointerKey(e) {
@@ -90,18 +73,16 @@ class Input {
   }
 
   static _onPointerDown(e) {
-    this.pointerCoor(e);
     let key = this.pointerKey(e);
     this._onKeyDown({ key, keyCode: 0 });
   }
 
   static _onPointerMove(e) {
-    this.pointerCoor(e);
 
   }
 
   static _onPointerUp(e) {
-    this.pointerCoor(e);
+
     let key = this.pointerKey(e);
     this._onKeyUp({ key, keyCode: 0 });
   }
@@ -109,29 +90,28 @@ class Input {
   static _onWheel(e) {
     this._pointer._wx += e.deltaX;
     this._pointer._wy += e.deltaY;
-
   }
 
-  static getMapper() {
-    return Params.mapper;
+  static get mapper(){
+    return this._mapper;
   }
 
-  static getPointer() {
+  static get pointer() {
     return { x, y, wx, wy } = this._pointer;
   }
 
-  /**
-   * 设置双击的判定范围，单位毫秒
-   * @param {any} value=200
-   */
-  static setRepeatWait(value = 200) {
-    this._repeatWait = value;
+  static get wheel() {
+    return { wx, wy } = this._pointer;
+  }
+
+  static set repeatInterval(value = 200) {
+    this._repeatInterval = value;
+  }
+  static get repeatInterval() {
+    return this._repeatInterval;
   }
 
   static update(delta) {
-    let now = performance.now();
-    // 真实时间
-    let d = now - this.now;
     // 更新未释放的
     this._currentState.forEach((pressing, key) => {
       if (pressing.press < 0) {
@@ -139,7 +119,7 @@ class Input {
         return;
       }
       // console.log(key, pressing);
-      pressing.press += d;
+      pressing.press += delta;
     })
     // 更新释放的
     this._gcing.forEach((pressing, key, gc) => {
@@ -147,8 +127,8 @@ class Input {
         pressing.press--;
         return;
       }
-      pressing.press -= d;
-      if (pressing.press <= -this._repeatWait) {
+      pressing.press -= delta;
+      if (pressing.press <= -this.repeatInterval) {
         gc.delete(key);
       }
     })
@@ -158,16 +138,7 @@ class Input {
 
     this._pointer._wx = 0;
     this._pointer._wy = 0;
-    // console.log(this._pointer);
-    this.now = now;
-    // console.log(this._currentState, this._gcing)
 
-  }
-
-  static pointerMoving() { // 搁置
-    const data = { x, y, lx, ly } = this._pointer;
-    if (x == lx && y == ly) return;
-    return data;
   }
 
   static isAnyTriggered(mouse = false) {
@@ -181,18 +152,12 @@ class Input {
     return find;
   }
 
-  static wheeling() {
-    if (!this._pointer.wx && !this._pointer.wy) return;
-    return { wx, wy } = this._pointer;
-  }
-
   static isTriggered(key) {
-    const data = this.getMapper()[key];
-    if (data) {
-      for (let k in data) {
-        let t = this.isTriggered(k);
-        if (t) return t;
-      }
+    const data = this.mapper[key];
+    if(!data) return;
+    for (let k in data) {
+      let t = this.isTriggered(k);
+      if (t) return t;
     }
     let state = this._currentState.get(key);
     return state && state.press == 0;
@@ -200,11 +165,10 @@ class Input {
 
   static isPressed(key, time = 300) {
     const data = this.getMapper()[key];
-    if (data) {
-      for (let k in data) {
-        let p = this.isPressed(k, time);
-        if (p) return p;
-      }
+    if(!data) return;
+    for (let k in data) {
+      let p = this.isPressed(k, time);
+      if (p) return p;
     }
     if (this._currentState) {
       this;
@@ -214,33 +178,38 @@ class Input {
   }
 
 
-  static pointerInside(x0 = 0, y0 = 0, w = 0, h = 0) {
-    let rect = new PIXI.Rectangle(x0, y0, w, h);
+  static hitTest(displayObject) {
+    let rect = displayObject.getBounds();
+    const { x, y } = this._pointer;
+    let inside = rect.contains(x, y);
+    return inside;
+  }
+
+  static hitTestRect(x, y, w, h) {
+    let rect = new Rectangle(x, y, w, h);
     const { x, y } = this._pointer;
     let inside = rect.contains(x, y);
     return inside;
   }
 
   static isReleased(key) {
-    const data = this.getMapper()[key];
-    if (data) {
-      for (let k in data) {
-        if (this.isPressed(k, 0)) return;
-        let r = this.isReleased(k);
-        if (r) return r;
-      }
+    const data = this.mapper[key];
+    if(!data) return;
+    for (let k in data) {
+      // if (this.isPressed(k, 0)) return;
+      let r = this.isReleased(k);
+      if (r) return r;
     }
     let state = this._gcing.get(key);
     return state && state.press == 0;
   }
 
-  static isRepeated(key, time = this._repeatWait) {
+  static isRepeated(key, time = this.repeatInterval) {
     const data = this.getMapper()[key];
-    if (data) {
-      for (let k in data) {
-        let r = this.isRepeated(k, time);
-        if (r) return r;
-      }
+    if(!data) return;
+    for (let k in data) {
+      let r = this.isRepeated(k, time);
+      if (r) return r;
     }
     let trigger = this.isTriggered(key);
     let gcing = this._gcing.get(key);
@@ -253,27 +222,6 @@ class Input {
     return trigger && gcing;
   }
 
-  static clampDirection(...mapping) {
-    this._dirClamp = {};
-    mapping.forEach((d, i, map) => {
-      if (i % 2) return;
-      this._dirClamp[d] = map[i + 1];
-    })
-  }
-
-  static change(motion, a = '', b = '', save = true) {
-    if (a == b) return;
-    let mapper = Params.mapper;
-    a = a.toLocaleLowerCase();
-    b = b.toLocaleLowerCase();
-    mapper[motion] = mapper[motion] || {};
-    delete mapper[motion][a];
-    b = b.toLocaleLowerCase();
-    mapper[motion][b] = true;
-    if (save) {
-      ConfigManager.keyMapper = Params.mapper;
-    }
-  }
 }
 
 export { Input }
