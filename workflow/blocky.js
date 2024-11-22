1c6b2cceb467ca46c61cd3c66b5ce718f0a7da63
MISAKI <33744048+AsakuraMisaki@users.noreply.github.com>
2024-11-22 19:51:19 +0800

    -

--- workflow/blocky.js
+++ workflow/blocky.js
@@ -50,18 +50,40 @@ class ToolboxBlockDescription{
 }
 
 // class statement [only for] code auto-complete
-class string_length extends Blockly.Block{ 
+class Queue_make extends Blockly.Block{ 
+  block(){ //Blockly.Blocks...init
+    this.appendEndRowInput("Label")
+        .appendField('创建序列')
+        .appendField(new Blockly.FieldTextInput("myQueue"), "Qname");
+    this.setNextStatement(true);
+    this.setColour(255);
+    this.setTooltip('创建序列');
+  }
+  forBlock(block=this, generator = PlainGenerator){ //generator.forBlock
+    let value = block.getFieldValue("Qname");
+    // console.log(value);
+    // // let next = 
+    // let next = block.getNextBlock()
+    return value;
+  }
+  //toolbox-xml-category-auto-detect
+  static toolbox(){
+    let desc = ToolboxBlockDescription();
+    desc.category = "Queue";
+  }
+}
+
+class Queue_relation extends Blockly.Block{ 
   block(){ //Blockly.Blocks...init
     this.appendValueInput('VALUE')
-        .setCheck('String')
+        .setCheck('Number')
         .appendField(new Blockly.FieldCheckbox(true), 'Amount')
         .appendField('精灵');
-    this.setOutput(true, 'Number');
+    // this.setOutput(true, 'Number');
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(160);
     this.setTooltip('Returns number of letters in the provided text.');
-    this.setHelpUrl('http://www.w3schools.com/jsref/jsref_length_string.asp');
   }
   forBlock(block=this, generator = PlainGenerator){ //generator.forBlock
     let value = block.getFieldValue("Amount");
@@ -77,9 +99,7 @@ class string_length extends Blockly.Block{
   }
 }
 
-Blockly.Blocks.string_length = {
-  init: string_length.prototype.block,
-}
+
 PlainGenerator.scrub_ = function(block, code, thisOnly) {
   const nextBlock =
       block.nextConnection && block.nextConnection.targetBlock();
@@ -88,7 +108,14 @@ PlainGenerator.scrub_ = function(block, code, thisOnly) {
   }
   return code;
 };
+Blockly.Blocks.string_length = {
+  init: string_length.prototype.block,
+}
 PlainGenerator.forBlock.string_length = string_length.prototype.forBlock.bind(string_length);
+Blockly.Blocks.make = {
+  init: make.prototype.block,
+}
+PlainGenerator.forBlock.make = make.prototype.forBlock.bind(make);
 
 // class targetToolBox extends Blockly.Toolbox{
 //   constructor(categoryDef, toolbox, opt_parent){
