4446b3a1ad311ec371826235930cc4211baf48aa
MISAKI <275684857@qq.com>
2024-11-24 21:58:51 +0800

    -

--- workflow/block.js
+++ workflow/block.js
@@ -78,8 +78,52 @@ class Queue_relation extends Blockly.Block{
     this.appendValueInput('VALUE')
         .setCheck('Number')
         .appendField(new Blockly.FieldCheckbox(true), 'Amount')
+        .appendField('序列关系');
+    this.setOutput(true, 'typeReturn');
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(160);
+    this.setTooltip('Returns number of letters in the provided text.');
+  }
+  forBlock(block=this, generator = PlainGenerator){ //generator.forBlock
+    let value = block.getFieldValue("Amount");
+    console.log(value);
+    // let next = 
+    let next = block.getNextBlock()
+    return value;
+  }
+  //toolbox-xml-category-auto-detect
+  static toolbox(){
+    let desc = ToolboxBlockDescription();
+    desc.category = "Queue";
+  }
+}
+
+class Queue_typeReturn extends Blockly.Block{ 
+  block(){ //Blockly.Blocks...init
+    this.appendValueInput('cacheName')
+        .setCheck('typeReturn')
+        .appendField(new Blockly.FieldTextInput("id"), 'cacheName')
+        .appendField(':缓存');
+  }
+  forBlock(block=this, generator = PlainGenerator){ //generator.forBlock
+    return 0;
+  }
+  //toolbox-xml-category-auto-detect
+  static toolbox(){
+    let desc = ToolboxBlockDescription();
+    desc.category = "Queue";
+  }
+}
+
+
+class Queue_run extends Blockly.Block{ 
+  block(){ //Blockly.Blocks...init
+    this.appendValueInput('VALUE')
+        .setCheck('Number')
+        .appendField(new Blockly.FieldCheckbox(true), 'Amount')
         .appendField('精灵');
-    // this.setOutput(true, 'Number');
+    this.setOutput(true, 'typeReturn');
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(160);
@@ -108,15 +152,23 @@ PlainGenerator.scrub_ = function(block, code, thisOnly) {
   }
   return code;
 };
-Blockly.Blocks.string_length = {
-  init: string_length.prototype.block,
+Blockly.Blocks.Queue_relation = {
+  init: Queue_relation.prototype.block,
 }
-PlainGenerator.forBlock.string_length = string_length.prototype.forBlock.bind(string_length);
-Blockly.Blocks.make = {
-  init: make.prototype.block,
+PlainGenerator.forBlock.Queue_relation = Queue_relation.prototype.forBlock.bind(Queue_relation);
+Blockly.Blocks.Queue_run = {
+  init: Queue_run.prototype.block,
 }
-PlainGenerator.forBlock.make = make.prototype.forBlock.bind(make);
+PlainGenerator.forBlock.Queue_run = Queue_run.prototype.forBlock.bind(Queue_run);
+Blockly.Blocks.Queue_make = {
+  init: Queue_make.prototype.block,
+}
+PlainGenerator.forBlock.Queue_make = Queue_make.prototype.forBlock.bind(Queue_make);
 
+Blockly.Blocks.Queue_typeReturn = {
+  init: Queue_typeReturn.prototype.block,
+}
+PlainGenerator.forBlock.Queue_typeReturn = Queue_typeReturn.prototype.forBlock.bind(Queue_typeReturn);
 // class targetToolBox extends Blockly.Toolbox{
 //   constructor(categoryDef, toolbox, opt_parent){
 //     super(categoryDef, toolbox, opt_parent);
